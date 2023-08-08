b5b0fc2c2d96f7672638a36fa15048550e4e779b
Iamneo <techadmin@iamneo.ai>
2023-08-08 07:20:10 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/TaskController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TaskController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.examly.springapp.service.TaskService;
+import com.examly.springapp.service.*;
 
 @RestController
 public class TaskController {
--- springapp/src/main/java/com/examly/springapp/service/TasksService.java
+++ springapp/src/main/java/com/examly/springapp/service/TasksService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.examly.springapp.model.Task;
-import com.examly.springapp.repository.TaskRepository;
+import com.examly.springapp.repository.*;
 
 @Service
 public class TasksService{
